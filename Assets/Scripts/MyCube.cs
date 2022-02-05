724c4794b270edfc5995e71f6cfe3ade164cfa53
Andrew Parker <adparker@gmail.com>
2022-02-05 12:14:10 -0800

    Added MyCube, updated MySphere, and deleted the Cylinder.

---
+++ Assets/Scripts/MyCube.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCube : MyShape
+{
+    void Awake()
+    {
+        msg = "I am a Cube.";
+    }
+ }
---
+++ Assets/Scripts/MySphere.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySphere : MyShape
+{
+    void Awake()
+    {
+        msg = "I am a Sphere.";
+    }
+}
