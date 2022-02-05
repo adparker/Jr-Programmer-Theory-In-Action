0cbe9a587bd39e5d28a907970fb1b5c3cc1f9d74
Andrew Parker <adparker@gmail.com>
2022-02-05 12:17:04 -0800

    Added comment for Inheritance.

--- Assets/Scripts/MyCube.cs
+++ Assets/Scripts/MyCube.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyCube : MyShape
+public class MyCube : MyShape // Inheritance. 
 {
     void Awake()
     {
--- Assets/Scripts/MySphere.cs
+++ Assets/Scripts/MySphere.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MySphere : MyShape
+public class MySphere : MyShape // Inheritance.
 {
     void Awake()
     {
